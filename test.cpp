55ee130246b055eb90505f82ac211c33d5a993d4
Genisys <17355439+guh0613@users.noreply.github.com>
2023-11-13 17:19:32 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,4 +1,6 @@
+#include <fstream>
 #define CPPHTTPLIB_OPENSSL_SUPPORT
+#define CPPHTTPLIB_ZLIB_SUPPORT
 
 #include "httplib.h"
 #include "json.hpp"
@@ -6,6 +8,7 @@
 #include <regex>
 #include <string>
 #include <ctime>
+#include <chrono>
 
 
 
@@ -31,8 +34,7 @@ Headers headers_code = {
     {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
     {"Accept-Encoding", "gzip, deflate, br, zstd"},
     {"Accept-Language", "zh-CN,zh;q=0.9"},
-    {"Cookie", "JSESSIONID=F40D5C27EF45F5EB87C06EA396B4DA8F; route=7e13d3d276af8b7437ce5afd5a9783e9; org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=zh_CN"},
-    {"Referer", "https,//authserver.nnu.edu.cn/authserver/login?type=qrcode&service=http%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html%23%2FWorkBench%2Fworkbench"},
+    {"Referer", "https://authserver.nnu.edu.cn/authserver/login?type=qrcode&service=http%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html%23%2FWorkBench%2Fworkbench"},
     {"Sec-Ch-Ua", "\"Not=A?Brand\";v=\"99\", \"Chromium\";v=\"118\""},
     {"Sec-Ch-Ua-Mobile", "?0"},
     {"Sec-Ch-Ua-Platform", "macOS"},
@@ -62,28 +64,54 @@ Headers headers = {
     {"Upgrade-Insecure-Requests", "1"},
     {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.90 Safari/537.36"}
 };
+time_t GetCurrentTimeMsec()
+{
+    using namespace std::chrono;
+    return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+
+}
 int getaccount(){
     string body;
     Client cli("https://authserver.nnu.edu.cn");
     auto res = cli.Get("/authserver/login?type=qrcode&service=http%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html%23%2FWorkBench%2Fworkbench", headers_first, 
   [&](const Response &response) {
+    string jsess,rout;
     for(auto value  : response.headers){
-        cout << value.first << " : " << value.second << endl;
-    }   
+        if (value.first == "Set-Cookie") {
+            smatch jsession,route;
+            
+            auto retroute = regex_search(value.second, route, regex("route=(.*?);"));
+            auto retjsession = regex_search(value.second, jsession, regex("JSESSIONID=(.*?);"));
+            if (retjsession) {
+                jsess = jsession[1];
+            }
+            if (retroute) {
+                rout = route[1];
+            }
+        }
+    }
+    headers_code.emplace("Cookie", "JSESSIONID=" + jsess + "; route=" + rout + "; org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=zh_CN");
     return true; 
   },
   [&](const char *data, size_t data_length) {
     body.append(data, data_length);
     return true;
   });
-    smatch m;
-    string execution = "";
-    auto ret = regex_search(body, m, regex("<input type=\"hidden\" name=\"execution\" value=\"(.*?)\" />"));
-    if(ret){
-        execution = m[1];
-    }
-    cout << ret << endl;
-    return  0;
+  cli.set_keep_alive(true);
+  smatch m;
+  string execution;
+  auto ret = regex_search(body, m, regex("<input type=\"hidden\" name=\"execution\" value=\"(.*?)\"/>"));
+  if(ret){
+      execution = m[1];
+  }
+  auto uuidget = cli.Get("/authserver/qrCode/getToken?ts="+to_string(GetCurrentTimeMsec()), headers_code);
+  cli.set_keep_alive(true);
+  string uuid = uuidget->body;
+  auto qrcode = cli.Get("/authserver/qrCode/getCode?uuid=" + uuid, headers_code);
+  ofstream ofs("./output.png", ios::binary);
+  ofs.write(qrcode->body.c_str(), qrcode->body.size());
+  ofs.close();
+  return 0;
 }
 int main(){
     getaccount();
