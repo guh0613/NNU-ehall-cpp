fee96afb4ab4579af4ceca8c35eb4a75db43cb35
Genisys <17355439+guh0613@users.noreply.github.com>
2023-11-24 00:20:33 +0800

    Update

--- login.cpp
+++ login.cpp
@@ -17,64 +17,115 @@ using json = nlohmann::json;
 using namespace httplib;
 using namespace std;
 
-Headers headers_first = {
+
+int saveCastgcToFile(string castgc, string name);
+string qrcodeTicketForFirstLogin(string name);
+string getWEU(string ticket);
+
+Headers headers_first_login = {
     {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
-    {"Accept-Encoding", "gzip, deflate, br, zstd"},
+    {"Accept-Encoding", "gzip, deflate, br"},
     {"Accept-Language", "zh-CN,zh;q=0.9"},
-    {"Referer", "https://authserver.nnu.edu.cn/authserver/login?type=qrcode&service=http%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html%23%2FWorkBench%2Fworkbench"},
-    {"Sec-Ch-Ua", "\"Not=A?Brand\";v=\"99\", \"Chromium\";v=\"118\""},
+    {"Host", "authserver.nnu.edu.cn"},
+    {"Upgrade-Insecure-Requests", "1"},
+    {"Referer", "https://ehall.nnu.edu.cn/"},
+    {"Sec-Ch-Ua", "\"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\""},
     {"Sec-Ch-Ua-Mobile", "?0"},
-    {"Sec-Ch-Ua-Platform", "macOS"},
+    {"Sec-Ch-Ua-Platform", "\"macOS\""},
     {"Sec-Fetch-Dest", "document"},
     {"Sec-Fetch-Mode", "navigate"},
     {"Sec-Fetch-Site", "same-origin"},
+    {"Priority", "u=0, i"},
+    {"Connection", "close"},
+    {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.90 Safari/537.36"}
+};
+Headers headers_second_login = {
+    {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
+    {"Accept-Encoding", "gzip, deflate, br"},
+    {"Accept-Language", "zh-CN,zh;q=0.9"},
+    {"Host", "authserver.nnu.edu.cn"},
+    {"Upgrade-Insecure-Requests", "1"},
+    {"Referer", "https://authserver.nnu.edu.cn/authserver/login?service=https%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html"},
+    {"Sec-Ch-Ua", "\"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\""},
+    {"Sec-Ch-Ua-Mobile", "?0"},
+    {"Sec-Ch-Ua-Platform", "\"macOS\""},
+    {"Sec-Fetch-Dest", "document"},
+    {"Sec-Fetch-Mode", "navigate"},
+    {"Sec-Fetch-Site", "same-origin"},
+    {"Priority", "u=0, i"},
+    {"Connection", "close"},
     {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.90 Safari/537.36"}
 };
 
 Headers headers_code = {
     {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
-    {"Accept-Encoding", "gzip, deflate, br, zstd"},
+    {"Accept-Encoding", "gzip, deflate, br"},
     {"Accept-Language", "zh-CN,zh;q=0.9"},
-    {"Referer", "https://authserver.nnu.edu.cn/authserver/login?type=qrcode&service=http%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html%23%2FWorkBench%2Fworkbench"},
-    {"Sec-Ch-Ua", "\"Not=A?Brand\";v=\"99\", \"Chromium\";v=\"118\""},
+    {"Upgrade-Insecure-Requests", "1"},
+    {"Referer", "https://authserver.nnu.edu.cn/authserver/login?type=qrcode&service=https%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html%23%2FWorkBench%2Fworkbench"},
+    {"Sec-Ch-Ua", "\"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\""},
     {"Sec-Ch-Ua-Mobile", "?0"},
-    {"Sec-Ch-Ua-Platform", "macOS"},
+    {"Sec-Ch-Ua-Platform", "\"macOS\""},
     {"Sec-Fetch-Dest", "document"},
     {"Sec-Fetch-Mode", "navigate"},
     {"Sec-Fetch-Site", "same-origin"},
     {"X-Requested-With", "XMLHttpRequest"},
+    {"Priority", "u=0, i"},
+    {"Connection", "close"},
     {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.90 Safari/537.36"}
 };
 
-Headers headers = {
+Headers headers_third_login = {
     {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
-    {"Accept-Encoding", "gzip, deflate, br, zstd"},
+    {"Accept-Encoding", "gzip, deflate, br"},
     {"Accept-Language", "zh-CN,zh;q=0.9"},
     {"Cache-Control", "max-age=0"},
+    {"Host", "authserver.nnu.edu.cn"},
     {"Content-Length", "5636"},
     {"Content-Type", "application/x-www-form-urlencoded"},
-    {"Origin", "http://authserver.nnu.edu.cn"},
-    {"Referer", "http://authserver.nnu.edu.cn/authserver/login?type=qrcode&service=http%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html%23%2FWorkBench%2Fworkbench"},
-    {"Sec-Ch-Ua", "\"Not=A?Brand\";v=\"99\", \"Chromium\";v=\"118\""},
+    {"Origin", "https://authserver.nnu.edu.cn"},
+    {"Referer", "https://authserver.nnu.edu.cn/authserver/login?type=qrcode&service=https%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html%23%2FWorkBench%2Fworkbench"},
+    {"Sec-Ch-Ua", "\"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\""},
     {"Sec-Ch-Ua-Mobile", "?0"},
-    {"Sec-Ch-Ua-Platform", "macOS"},
+    {"Sec-Ch-Ua-Platform", "\"macOS\""},
     {"Sec-Fetch-Dest", "document"},
     {"Sec-Fetch-Mode", "navigate"},
     {"Sec-Fetch-Site", "same-origin"},
     {"Upgrade-Insecure-Requests", "1"},
+    {"Sec-Fetch-User", "?1"},
+    {"Priority", "u=0, i"},
+    {"Connection", "close"},
     {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.90 Safari/537.36"}
 };
+Headers headers_verify = {
+{"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
+    {"Accept-Encoding", "gzip, deflate, br"},
+    {"Accept-Language", "zh-CN,zh;q=0.9"},
+    {"Cache-Control", "max-age=0"},
+    {"Host", "ehall.nnu.edu.cn"},
+    {"Cookie", "route=d68a40386be0931ea6cbdda4cfe3192b; JSESSIONID=GWL8ryp6XssPLARVSnuFzKAB1QmYzL8jnsvV3JANpmBJxvNa-Wmv!-750103191; amp.locale=zh_CN"},
+    {"Referer", "https://authserver.nnu.edu.cn/"},
+    {"Sec-Ch-Ua", "\"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\""},
+    {"Sec-Ch-Ua-Mobile", "?0"},
+    {"Sec-Ch-Ua-Platform", "\"macOS\""},
+    {"Sec-Fetch-Dest", "document"},
+    {"Sec-Fetch-Mode", "navigate"},
+    {"Sec-Fetch-Site", "same-origin"},
+    {"Upgrade-Insecure-Requests", "1"},
+    {"Priority", "u=0, i"},
+    {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.90 Safari/537.36"}
+};
+
 time_t GetCurrentTimeMsec()
 {
     using namespace std::chrono;
     return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
 
 }
-int getaccount(){
-    string body;
-    Client cli("https://authserver.nnu.edu.cn");
-    cli.set_keep_alive(true);
-    auto res = cli.Get("/authserver/login?type=qrcode&service=http%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html%23%2FWorkBench%2Fworkbench", headers_first, 
+string qrcodeTicketForFirstLogin(string name){
+    string body,body_second;
+    Client cli("https://authserver.nnu.edu.cn:443");
+    auto res = cli.Get("/authserver/login?service=https%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html", headers_first_login, 
   [&](const Response &response) {
     string jsess,rout;
     for(auto value  : response.headers){
@@ -92,16 +143,25 @@ int getaccount(){
         }
     }
     headers_code.emplace("Cookie", "JSESSIONID=" + jsess + "; route=" + rout + "; org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=zh_CN");
-    headers.emplace("Cookie", "JSESSIONID=" + jsess + "; route=" + rout + "; org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=zh_CN");
+    headers_second_login.emplace("Cookie", "JSESSIONID=" + jsess + "; route=" + rout + "; org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=zh_CN");
+    headers_third_login.emplace("Cookie", "JSESSIONID=" + jsess + "; route=" + rout + "; org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=zh_CN");
     return true; 
   },
   [&](const char *data, size_t data_length) {
     body.append(data, data_length);
     return true;
   });
+  auto secondloginres = cli.Get("/authserver/login?type=qrcode&service=https%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html%23%2FWorkBench%2Fworkbench",headers_second_login,
+  [&](const Response &response) {
+    return true; 
+  },
+  [&](const char *data, size_t data_length) {
+    body_second.append(data, data_length);
+    return true;
+  });
   smatch m;
   string execution;
-  auto ret = regex_search(body, m, regex("<input type=\"hidden\" name=\"execution\" value=\"(.*?)\"/>"));
+  auto ret = regex_search(body_second, m, regex("<input type=\"hidden\" name=\"execution\" value=\"(.*?)\"/>"));
   if(ret){
       execution = m[1];
   }
@@ -111,7 +171,9 @@ int getaccount(){
   ofstream ofs("./output.png", ios::binary);
   ofs.write(qrcode->body.c_str(), qrcode->body.size());
   ofs.close();
-  scanf("是否扫码完成？");
+  printf("%s", "请扫码登录,扫码登录完成后，请按下键盘上的任意键");
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
         Params params{
             {"uuid", uuid},
             {"execution", execution},
@@ -122,37 +184,159 @@ int getaccount(){
             {"rmShown", "1"}
         };
         cli.set_keep_alive(false);
-        auto loginres = cli.Post("/authserver/login?display=qrLogin&service=http%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html%23%2FWorkBench%2Fworkbench", headers, params);
+        auto loginres = cli.Post("/authserver/login?display=qrLogin&service=https%3A%2F%2Fehall.nnu.edu.cn%2Flogin%3Fservice%3Dhttp%3A%2F%2Fehall.nnu.edu.cn%2Fywtb-portal%2Fstandard%2Findex.html%23%2FWorkBench%2Fworkbench", headers_third_login, params);
         string newurl;
+        string newpath;
         for (auto value : loginres->headers) {
+            
             if (value.first == "Location") {
                 newurl = value.second;
+                cout << newurl << endl;
+            }
+            if (value.first == "Set-Cookie") {
+                string cookie = value.second;
+                smatch castgcm;
+                auto ret = regex_search(cookie, castgcm, regex("CASTGC=(.*?);"));
+                if (ret) {
+                    saveCastgcToFile(castgcm[1], name);
+                }
+
             }
         }
         string ticket;
         smatch m2;
-        //http://ehall.nnu.edu.cn/login?service=http://ehall.nnu.edu.cn/ywtb-portal/standard/index.html&ticket=ST-2633833-1eyFK59FZiVRAORN86hi0fe7BQAids7authserver4#/WorkBench/workbench
-
+        //https://ehall.nnu.edu.cn/login?service=https://ehall.nnu.edu.cn/ywtb-portal/standard/index.html&ticket=ST-2633833-1eyFK59FZiVRAORN86hi0fe7BQAids7authserver4#/WorkBench/workbench
+        //we need the path which is the content after the domain name
         auto ret2 = regex_search(newurl, m2, regex("ticket=(.*?)#"));
         if (ret2) {
             ticket = m2[1];
         }
-        cout<<ticket<<endl;
-        Client cli2("http://ehall.nnu.edu.cn");
-        auto nnures = cli2.Get("/login?service=http://ehall.nnu.edu.cn/ywtb-portal/standard/index.html&ticket=" + ticket + "#/WorkBench/workbench",headers);
-        for (auto value : nnures->headers) {
-            cout << value.first << ":" << value.second << endl;
-        
-        }
+
+        Client cli2("https://ehall.nnu.edu.cn");
+        cli2.Get("/login?service=http://ehall.nnu.edu.cn/ywtb-portal/standard/index.html&ticket="+ticket, headers_verify);
         //cli.set_follow_location(true);
         //auto nnures2 = cli.Get("/",headers);
 
-  return 0;
+  return ticket;
 }
-int main(){
-    getaccount();
-    return 0;
+int saveCastgcToFile(string castgc, string name){
+    const string DATA_PATH = "./data/login_tgc.json";
+    json j;
+    //if the file and folder does not exist, create it,or open it;
+    //then save the new tgc json data to the file
+    ifstream ifs(DATA_PATH);
+    if (!ifs) {
+        j[name] = castgc;
+        ofstream ofs(DATA_PATH);
+        ofs << j;
+        ofs.close();
+        return 0;
+    } else {
+        //do not load if the file is empty
+        if (ifs.peek() == EOF) {
+            ifs.close();
+            j[name] = castgc;
+            ofstream ofs(DATA_PATH);
+            ofs << j;
+            ofs.close();
+            return 0;
+        } else {
+        j = json::parse(ifs);
+        ifs.close();
+        j[name] = castgc;
+        ofstream ofs(DATA_PATH);
+        ofs << j;
+        ofs.close();
+        return 0;
+        }
+    }
+    
+
+    return 1;
 }
 
+string getWEU(string ticket){
+    string path,body,body2,weu_long= "";
+    Headers headers_appshow = {
+    {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
+    {"Accept-Encoding", "gzip, deflate, br"},
+    {"Accept-Language", "zh-CN,zh;q=0.9"},
+    {"Cache-Control", "max-age=0"},
+    {"Host", "ehall.nnu.edu.cn"},
+    {"Sec-Ch-Ua", "\"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\""},
+    {"Sec-Ch-Ua-Mobile", "?0"},
+    {"Sec-Ch-Ua-Platform", "\"macOS\""},
+    {"Sec-Fetch-Dest", "document"},
+    {"Sec-Fetch-Mode", "navigate"},
+    {"Sec-Fetch-Site", "same-origin"},
+    {"Upgrade-Insecure-Requests", "1"},
+    {"Priority", "u=0, i"},
+    {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.90 Safari/537.36"}
+};
+Headers headers_weu = {
+    {"Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
+    {"Accept-Encoding", "gzip, deflate, br"},
+    {"Accept-Language", "zh-CN,zh;q=0.9"},
+    {"Cache-Control", "max-age=0"},
+    {"Host", "ehallapp.nnu.edu.cn"},
+    {"Sec-Ch-Ua", "\"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\""},
+    {"Sec-Ch-Ua-Mobile", "?0"},
+    {"Sec-Ch-Ua-Platform", "\"macOS\""},
+    {"Sec-Fetch-Dest", "document"},
+    {"Sec-Fetch-Mode", "navigate"},
+    {"Sec-Fetch-Site", "same-origin"},
+    {"Upgrade-Insecure-Requests", "1"},
+    {"Priority", "u=0, i"},
+    {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.5993.90 Safari/537.36"}
+};
+  headers_appshow.emplace("Cookie", "MOD_AUTH_CAS=MOD_AUTH_"+ticket+"; asessionid=9b7b1f8c-a17e-4a82-ac48-586362fc9117; amp.locale=zh; JSESSIONID=xTX77bTzr2YPuNS9LsfY5FjSXHXgL2fNc3LThTQdkmO06Blb--k0!-750103190; route=34e12749345e5a1ad7cc6d0324fa3b1c");
+  headers_weu.emplace("Cookie", "MOD_AUTH_CAS=MOD_AUTH_"+ticket+"; route=34e12749345e5a1ad7cc6d0324fa3b1c");
+  Client cli("https://ehall.nnu.edu.cn");
+  auto res = cli.Get("/appShow?appId=4768687067472349", headers_appshow, 
+  [&](const Response &response) {
+    for(auto value  : response.headers){
+        cout << value.first << ":" << value.second << endl;
+      if(response.status == 302 && value.first == "Location"){
+        string location = value.second;
+        //Location: http://ehallapp.nnu.edu.cn/jwapp/sys/studentWdksapApp/*default/index.do?t_s=1700738745482&amp_sec_version_=1&gid_=WGhYYWREa2ZBczZBZnNrdTF2aEhma2lWbnlnd1IxT21ubUpoaWtZeHdKVmZlQUVKalQrSzBmUm5NQ1FPUkR1ditON1E5SFc4dTV1cEF6aW1UamRCSFE9PQ&EMAP_LANG=zh&THEME=teal
+        regex pattern("/jwapp.*");
+        smatch result;
+        regex_search(location, result, pattern);
+        path = result[0];
+        cout << "path:" << path << endl;
+      }
+    }
+    return true; 
+  },
+  [&](const char *data, size_t data_length) {
+    body.append(data, data_length);
+    return true;
+});
+    Client cli2("https://ehallapp.nnu.edu.cn");
+    auto res2 = cli2.Get(path, headers_weu,
+    [&](const Response &response2) {
+    //we want the long one, not the short one
+    for(auto value2  : response2.headers){
+        if (value2.first == "Set-Cookie"){
+            string cookie = value2.second;
+            regex pattern("_WEU=([^;]+\\.)");
+            smatch result;
+            regex_search(cookie, result, pattern);
+            string weu = result[1];
+            if (weu.length() > weu_long.length()){
+                weu_long = weu;
+                
+            }
+
+        }
+    }
+    return true; 
+  },
+  [&](const char *data, size_t data_length) {
+    body2.append(data, data_length);
+    return true;
+  });
+  return weu_long;
+}
 
 
--- queryinfo.cpp
+++ queryinfo.cpp
@@ -1,3 +1,7 @@
+#include <cstdio>
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#define CPPHTTPLIB_ZLIB_SUPPORT
+
 #include "httplib.h"
 #include "json.hpp"
 #include <iostream>
@@ -6,69 +10,99 @@
 using json = nlohmann::json;
 using namespace std;
 
-namespace getinfo {
+
+int getunreadmessage(string ticket);
+string getstudentname(string ticket);
+string getmailmessage(string ticket);
+int getexamall(string ticket, string weu);
+int getexamarranged(string ticket, string weu);
+
+
 httplib::Headers headers = {
   {"Accept", "application/json, text/plain, */*"},
   {"Accept-Encoding", "gzip, deflate"},
-  {"Connection", "keep-alive"},
+  {"Connection", "close"},
+  {"Content-Type", "application/json"},
+  {"Host", "ehall.nnu.edu.cn"},
   {"User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15"},
-  {"Referer", "http://ehall.nnu.edu.cn/ywtb-portal/standard/index.html"},
+  {"Referer", "https://ehall.nnu.edu.cn/ywtb-portal/standard/index.html"},
   {"Accept-Language", "zh-CN,zh-Hans;q=0.9"},
   {"X-Requested-With", "XMLHttpRequest"},
+  {"Sec-Fetch-Dest", "empty"},
+  {"Sec-Fetch-Mode", "cors"},
+  {"Sec-Fetch-Site", "same-origin"},
+  {"Sec-Ch-Ua", "\"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\""},
+  {"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.6045.159 Safari/537.36"}
 };
 
 httplib::Headers headersapp = {
   {"Accept", "application/json, text/javascript, */*; q=0.01"},
-  {"Accept-Encoding", "gzip, deflate"},
+  {"Accept-Encoding", "gzip, deflate, br"},
   {"Accept-Language", "zh-CN,zh-Hans;q=0.9"},
-  {"Content-Length", "18"},
-  {"Content-Type", "application/x-www-form-urlencoded; charset=UTF-8"},
   {"Host", "ehallapp.nnu.edu.cn"},
-  {"Origin", "http://ehallapp.nnu.edu.cn"},
-  {"Connection", "keep-alive"},
+  {"Connection", "close"},
+  {"Origin", "https://ehallapp.nnu.edu.cn"},
   {"User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15"},
-  {"Referer", "http://ehallapp.nnu.edu.cn/jwapp/sys/studentWdksapApp/*default/index.do?t_s=1698567688342&amp_sec_version_=1&gid_=WGhYYWREa2ZBczZBZnNrdTF2aEhma2lWbnlnd1IxT21ubUpoaWtZeHdKVmZlQUVKalQrSzBmUm5NQ1FPUkR1ditON1E5SFc4dTV1cEF6aW1UamRCSFE9PQ&EMAP_LANG=zh&THEME=teal"},
+  {"Referer", "https://ehallapp.nnu.edu.cn/jwapp/sys/studentWdksapApp/*default/index.do?t_s=1700753954664&amp_sec_version_=1&gid_=WGhYYWREa2ZBczZBZnNrdTF2aEhma2lWbnlnd1IxT21ubUpoaWtZeHdKVmZlQUVKalQrSzBmUm5NQ1FPUkR1ditON1E5SFc4dTV1cEF6aW1UamRCSFE9PQ&EMAP_LANG=zh&THEME=teal"},
   {"Accept-Language", "zh-CN,zh-Hans;q=0.9"},
-  {"X-Requested-With", "XMLHttpRequest"},
+{"Priority", "u=1, i"},
+{"Sec-Fetch-Dest", "empty"},
+{"Sec-Fetch-Mode", "cors"},
+{"Sec-Fetch-Site", "same-origin"},
+{"Sec-Ch-Ua", "\"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\""},
+{"Sec-Ch-Ua-Mobile", "?0"},
+{"Sec-Ch-Ua-Platform", "\"macOS\""},
+  {"X-Requested-With", "XMLHttpRequest"}
 };
 
 
 
-int getunreadmessage(){
-httplib::Client cli("http://ehall.nnu.edu.cn");
+int getunreadmessage(string ticket){
+  //Cookie: route=34e12749345e5a1ad7cc6d0324fa3b1c; JSESSIONID=rOv77UsOCI-c0QrfjDs4dj71VP9wieuAoSHcEfBzedJaql6LejLF!-1803388596; amp.locale=zh_CN; MOD_AUTH_CAS=MOD_AUTH_ST-21803-ACbgeAofbuNmB95YLckhthR8u8Eids7authserver1; asessionid=9b7b1f8c-a17e-4a82-ac48-586362fc9117
+  headers.emplace("Cookie", "route=34e12749345e5a1ad7cc6d0324fa3b1c; JSESSIONID=rOv77UsOCI-c0QrfjDs4dj71VP9wieuAoSHcEfBzedJaql6LejLF!-1803388596; amp.locale=zh_CN; MOD_AUTH_CAS=MOD_AUTH_"+ticket+"; asessionid=9b7b1f8c-a17e-4a82-ac48-586362fc9117");
+httplib::Client cli("https://ehall.nnu.edu.cn");
   auto res = cli.Get("/jsonp/ywtb/getUnreadMessageCount", headers);
   json j = json::parse(res->body);
   return j["data"]["totalSize"];
 }
 
-string getstudentname(){
-  httplib::Client cli("http://ehall.nnu.edu.cn");
+string getstudentname(string ticket){
+  headers.emplace("Cookie", "route=34e12749345e5a1ad7cc6d0324fa3b1c; JSESSIONID=rOv77UsOCI-c0QrfjDs4dj71VP9wieuAoSHcEfBzedJaql6LejLF!-1803388596; amp.locale=zh_CN; MOD_AUTH_CAS=MOD_AUTH_"+ticket+"; asessionid=9b7b1f8c-a17e-4a82-ac48-586362fc9117");
+  httplib::Client cli("https://ehall.nnu.edu.cn");
   auto res = cli.Get("/jsonp/ywtb/info/getUserInfoAndSchoolInfo", headers);
   json j = json::parse(res->body);
   return j["data"]["userName"];
 }
 
 
-string getmailmessage(){
-  httplib::Client cli("http://ehall.nnu.edu.cn");
+string getmailmessage(string ticket){
+  headers.emplace("Cookie", "route=34e12749345e5a1ad7cc6d0324fa3b1c; JSESSIONID=rOv77UsOCI-c0QrfjDs4dj71VP9wieuAoSHcEfBzedJaql6LejLF!-1803388596; amp.locale=zh_CN; MOD_AUTH_CAS=MOD_AUTH_"+ticket+"; asessionid=9b7b1f8c-a17e-4a82-ac48-586362fc9117");
+  httplib::Client cli("https://ehall.nnu.edu.cn");
   auto res = cli.Get("/jsonp/ywtb/card/getViewDataDetail?wid=cd02610dafaf42cb8f6da5632c317669", headers);
   json j = json::parse(res->body);
   return j["data"][0]["imptInfo"];
 }
 
-int getexamall(){
-  httplib::Client cli("http://ehallapp.nnu.edu.cn");
+int getexamall(string ticket, string weu){
+  //Cookie: EMAP_LANG=zh; THEME=teal; _WEU=bddmyN6TbRF9KGJpXMgZB4Fcdac2Gn387AUMLmWa4FXOcfQ8Tk5rvyiQb9_NTk4iwg5PkJy5f2KZG0Aksq6kvXddMwSXzMy0S4VEDQfQ2Awkk8wXH2G9lo..; JSESSIONID=6F1B2EA14F85DCC7695B45FCB5B15053; MOD_AUTH_CAS=MOD_AUTH_ST-21806-a1p-wXA-Db3Jaxdp6w7hQqqHLXIids7authserver1; route=160cbea5c41d2df27b3fe53ecacb10e9
+  headersapp.emplace("Cookie", "EMAP_LANG=zh; THEME=teal; _WEU="+weu+"; JSESSIONID=6F1B2EA14F85DCC7695B45FCB5B15053; MOD_AUTH_CAS=MOD_AUTH_"+ticket+"; route=160cbea5c41d2df27b3fe53ecacb10e9");
+  httplib::Client cli("https://ehallapp.nnu.edu.cn");
   auto res = cli.Post("/jwapp/sys/studentWdksapApp/modules/wdksap/cxyxkwapkwdkc.do", headersapp, "XNXQDM=2023-2024-1", "application/x-www-form-urlencoded; charset=UTF-8");
   json j = json::parse(res->body);
   return j["datas"]["cxyxkwapkwdkc"]["totalSize"];
   
 }
 
-int getexamarranged(){
-  httplib::Client cli("http://ehallapp.nnu.edu.cn");
+int getexamarranged(string ticket, string weu){
+  headersapp.emplace("Cookie", "EMAP_LANG=zh; THEME=teal; _WEU="+weu+"; JSESSIONID=31CB3CC1A8076B54F1E51950AC53D203; MOD_AUTH_CAS=MOD_AUTH_"+ticket+"; route=7790156147990f68c998a9cea94374a5");
+  httplib::Client cli("https://ehallapp.nnu.edu.cn");
   auto res = cli.Post("/jwapp/sys/studentWdksapApp/modules/wdksap/wdksap.do", headersapp, "XNXQDM=2023-2024-1", "application/x-www-form-urlencoded; charset=UTF-8");
   json j = json::parse(res->body);
   return j["datas"]["wdksap"]["totalSize"];
 }
-}
+
+
+
+
+
   
